49e925782f736ecbdfd69bbd4b4eb95a2306f254
ultra-zx <71925857+ultra-zx@users.noreply.github.com>
2020-09-26 18:16:07 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,59 @@
+basic.forever(function () {
+    basic.pause(5000)
+    basic.showIcon(IconNames.Diamond)
+    basic.pause(5000)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.pause(5000)
+    basic.showIcon(IconNames.Square)
+    basic.pause(5000)
+    basic.showIcon(IconNames.Scissors)
+    basic.pause(5000)
+    basic.showIcon(IconNames.Ghost)
+    basic.pause(5000)
+    basic.showIcon(IconNames.Butterfly)
+    basic.pause(5000)
+    basic.showIcon(IconNames.Meh)
+    basic.pause(5000)
+    basic.showIcon(IconNames.Cow)
+    basic.pause(5000)
+    basic.showIcon(IconNames.Triangle)
+    basic.pause(5000)
+    basic.showIcon(IconNames.LeftTriangle)
+    basic.pause(5000)
+    basic.showIcon(IconNames.Sword)
+    basic.pause(5000)
+    basic.showArrow(ArrowNames.SouthWest)
+    basic.pause(5000)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(5000)
+    basic.showString("Indoraptor")
+    basic.pause(5000)
+    basic.showString("Stegosaurus")
+    basic.pause(5000)
+    basic.showString("Anklyosaurus")
+    basic.pause(5000)
+    basic.showString("Triceretops")
+    basic.pause(5000)
+})
